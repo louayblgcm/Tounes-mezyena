be531cd0c1a94aa9672f7a307f3fa35f767eb91f
louayblgcm <58984276+louayblgcm@users.noreply.github.com>
2020-10-13 00:17:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var next= document.getElementById("next");
 var myimages=["https://lp-cms-production.imgix.net/2019-06/e41ea44f84a51473b215065226625602-tunisia.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4","https://miro.medium.com/max/9362/1*iUaWGAoNEao_JCGMjD40Qw.jpeg",
 "https://i.huffpost.com/gen/2778636/images/o-TUNISIA-facebook.jpg",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTw1hPVRRRfJFBCPstLiRQFPzbeHUON6DHXAYaxT27C2GBF5pfY",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6sveoT72DuU52kVj311H9BG6qz1GKul3IECBpB5-n684W1w3Z","https://www.facebook.com/photo.php?fbid=525235457825771&set=pb.100010179638601.-2207520000..&type=3"];
+"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6sveoT72DuU52kVj311H9BG6qz1GKul3IECBpB5-n684W1w3Z","https://www.facebook.com/photo.php?fbid=525235457825771&set=pb.100010179638601.-2207520000..&type=3",];
 image.setAttribute("src",myimages[4]);
 var myindex=0;
 prev.addEventListener('click',prevfn);
